a536e88d59f73ed41b2052c829fdff7f5d949a69
Adam Pollack <apollack11@gmail.com>
2017-01-28 16:37:09 -0600

    Completed programming assignment 7

--- machine-learning-ex7/ex7/findClosestCentroids.m
+++ machine-learning-ex7/ex7/findClosestCentroids.m
@@ -21,10 +21,10 @@ idx = zeros(size(X,1), 1);
 % Note: You can use a for-loop over the examples to compute this.
 %
 
-
-
-
-
+m = size(X,1);
+for i = 1:m
+    [~,idx(i,1)] = min(sum(((bsxfun(@minus,X(i,:),centroids)).^2),2));
+end
 
 
 % =============================================================
